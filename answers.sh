750b92613ab4b1b31308e0b1767f4bddab9c9ea6
Rebecca Chen <rebecca7@tamu.edu>
2024-10-09 13:48:44 -0500

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -1,10 +1,11 @@
-SELECT Name FROM Students;
-SELECT * FROM Students WHERE Age > 30;
-SELECT Name FROM Students WHERE Gender='F' AND Age=30;
-SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
-UPDATE Students SET Points = 500 WHERE Name='Basma';
-UPDATE Students SET Points = 100 WHERE Name='Alex';
+SELECT name FROM students;
+SELECT * FROM students WHERE age > 30;
+SELECT name FROM students WHERE gender = 'F' AND age = 30;
+SELECT points FROM students WHERE name = 'Alex';
+INSERT INTO students(id, name, age, gender, points)
+VALUES (7, 'Rebecca', 20, 'F', 200);
+UPDATE students SET points = 400 WHERE name = 'Basma';
+UPDATE students SET points = 100 WHERE name = 'Alex';
 
 CREATE TABLE graduates(
     ID INTEGER  PRIMARY KEY AUTOINCREMENT,
